b044a616a24b06fd81dd96c116d36fc97f03670e
Gabriel Robertson <gabrielrobertson.s@gmail.com>
2023-11-30 13:03:11 -0300

    contagemregressiva

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,45 +8,95 @@ const startPauseBt = document.querySelector('#start-pause')
 
 const tempoNaTela = document.querySelector('#timer')
 
+const audiofim = new Audio ('assets/sons/alarm-kitchen.mp3')
 
 let tempoDecorridoEmsegundos = 1500
-
-focoBt.addEventListener('click', () =>{
-    tempoDecorridoEmsegundos = 1500
+let intervaloId = null
+function foco(){
+    tempoDecorridoEmsegundos = 15
     alterarContexto('foco')
     focoBt.classList.add('active')
     
-})
+}
 
-curtoBt.addEventListener('click', () =>{
-    tempoDecorridoEmsegundos = 300
+focoBt.addEventListener('click', foco)
+
+
+
+function curto() {
+    tempoDecorridoEmsegundos = 3
     alterarContexto('curto')
     curtoBt.classList.add('active')
     console.log('Test2')
-    
+}
 
-})
+curtoBt.addEventListener('click', curto)
 
-longoBt.addEventListener('click', () =>{
-    tempoDecorridoEmsegundos = 900
+function longo() {
+    tempoDecorridoEmsegundos = 9
     alterarContexto('longo')
     longoBt.classList.add('active')
     console.log('Test3')
-    
 
-})
+
+}
+
+longoBt.addEventListener('click', longo)
+    
 
 function alterarContexto(contexto){
     mostrarTempo()
     botao.forEach(function (contexto){
         contexto.classList.remove('active')
     })
+    html.setAttribute('data-contexto', contexto)
+}
+
+let contagemFoco = 0
+
+const contagemRegressiva = () =>{
+    if (tempoDecorridoEmsegundos <= 0){
+        audiofim.play()
+        
+        const focoAtivo = html.getAttribute('data-contexto')
+        console.log(focoAtivo)
+        if (focoAtivo == 'foco'){   
+            contagemFoco++
+            console.log(contagemFoco)
+            if (contagemFoco == 4){
+                contagemFoco = 0
+                longo()
+            }else{
+                curto()
+            }
+
+        }else{
+            foco()
+        }
+
+        zerar()
+        return
+    }
+   tempoDecorridoEmsegundos -=1
+   mostrarTempo()
 }
 
 startPauseBt.addEventListener('click', iniciarOuPausar)
 
+
 function iniciarOuPausar(){
-    alert('Olá mené')
+    if (intervaloId){
+        zerar()
+        return
+    }
+    intervaloId = setInterval(contagemRegressiva, 1000)
+    startPauseBt.textContent = 'Pause'
+}
+
+function zerar(){
+    clearInterval(intervaloId)
+    startPauseBt.textContent = 'Start'
+    intervaloId = null
 }
 
 function mostrarTempo(){
@@ -57,4 +107,4 @@ function mostrarTempo(){
 
 mostrarTempo()
 
-alert('Site em Construção')
+// alert('Site em Construção')
